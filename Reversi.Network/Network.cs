1b3eed16b97e012fdb410768c8a8a2f2ce78539a
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-06-25 00:06:07 +0300

    feat(network): add handshake and Host.Port property for OS-assigned ports

--- Reversi.Network/Network.cs
+++ Reversi.Network/Network.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using Reversi.Core;
 using Reversi.Network.Protocol;
 
 namespace Reversi.Network;
@@ -10,6 +11,10 @@ namespace Reversi.Network;
 public abstract class Network
 {
     protected NetworkConnection? Connection;
+
+    /// <summary>
+    /// Gets a value indicating whether the network connection is currently active and established.
+    /// </summary>
     public bool Connected => Connection != null;
 
     /// <summary>
@@ -72,16 +77,30 @@ public abstract class Network
 /// Represents a network host (server) that listens for and accepts an incoming connection from a client.
 /// </summary>
 /// <param name="ipAddress">The local <see cref="IPAddress"/> to bind the listener to.</param>
-/// <param name="port">The port number on which to listen for incoming connection requests.</param>
+/// <param name="port">The port number on which to listen for incoming connection requests. Use 0 to let the OS assign a free port dynamically.</param>
 public class Host(IPAddress ipAddress, int port) : Network
 {
     private readonly TcpListener _listener = new(ipAddress, port);
 
     /// <summary>
-    /// Starts the listener and blocks execution until a remote client connects.
+    /// Gets the actual port number the listener is bound to. Useful when port 0 is provided during initialization.
     /// </summary>
-    /// <exception cref="NetworkErrorException">Thrown when a socket error occurs while starting the listener or accepting the client.</exception>
-    public void AcceptConnection()
+    /// <remarks>
+    /// Only valid after <see cref="AcceptConnection"/> has been called.
+    /// Accessing this property before calling <see cref="AcceptConnection"/> will throw an <see cref="InvalidOperationException"/>.
+    /// </remarks>
+    public int Port => ((IPEndPoint)_listener.LocalEndpoint).Port;
+
+    /// <summary>
+    /// Starts the listener, blocks execution until a remote client connects,
+    /// and performs an application-level handshake by sending <see cref="AcceptConnectMessage"/>.
+    /// </summary>
+    /// <param name="opponentColor">The <see cref="Player"/> color to be assigned and sent to the connecting client.</param>
+    /// <exception cref="NetworkErrorException">
+    /// Thrown when a socket error occurs while starting the listener or accepting the client,
+    /// or when an I/O error occurs while sending the handshake message.
+    /// </exception>
+    public void AcceptConnection(Player opponentColor)
     {
         try
         {
@@ -93,6 +112,8 @@ public class Host(IPAddress ipAddress, int port) : Network
 
             var stream = client.GetStream();
             Connection = new NetworkConnection(stream);
+
+            Send(new AcceptConnectMessage(opponentColor));
         }
         catch (SocketException e)
         {
@@ -109,20 +130,28 @@ public class Client : Network
     private readonly TcpClient _client = new();
 
     /// <summary>
-    /// Establishes a connection to a remote host at the specified IP address and port.
+    /// Establishes a connection to a remote host, performs the application-level handshake,
+    /// and retrieves the assigned player color.
     /// </summary>
     /// <param name="ipAddress">The <see cref="IPAddress"/> of the remote host.</param>
     /// <param name="port">The port number of the remote host.</param>
+    /// <returns>The <see cref="Player"/> color assigned to this client by the host.</returns>
     /// <exception cref="NetworkErrorException">Thrown when a socket error occurs while attempting to connect to the host.</exception>
-    public void Connect(IPAddress ipAddress, int port)
+    /// <exception cref="ProtocolException">Thrown if the host sends an unexpected message instead of the <see cref="AcceptConnectMessage"/> handshake.</exception>
+    /// <exception cref="DisconnectException">Thrown if the host drops the connection before completing the handshake.</exception>
+    public Player Connect(IPAddress ipAddress, int port)
     {
         try
         {
             _client.Connect(ipAddress, port);
-            _client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
 
             var stream = _client.GetStream();
             Connection = new NetworkConnection(stream);
+
+            var result = ReceiveMessage<AcceptConnectMessage>().Player;
+            _client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+            return result;
         }
         catch (SocketException e)
         {
