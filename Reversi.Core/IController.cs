82504c5478c319beec6f4b919a98ebc52af7a883
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-06-27 22:24:20 +0300

    refactor(core): remove legacy, add documentation for IView and IController

--- Reversi.Core/IController.cs
+++ Reversi.Core/IController.cs
@@ -2,6 +2,21 @@ namespace Reversi.Core;
 
 public interface IController
 {
+    /// <summary>
+    /// Запускає одну ігрову сесію від початку до кінця.
+    /// Щоразу створює новий екземпляр моделі всередині.
+    /// </summary>
+    /// <exception cref="Exception">Поки повний список не буде додано в документацію, можна перехоплювати базовий клас.</exception>
+    /// <param name="gameSettings">Налаштування для ініціалізації ігрової сесії.</param>
+    /// <param name="drawGame">
+    /// Колбек який викликається контролером для відображення поточного стану гри.
+    /// View відповідає за виведення стану користувачу.
+    /// </param>
+    /// <param name="askMove">
+    /// Колбек який викликається контролером для отримання ходу від активного гравця.
+    /// View отримує список валідних ходів і повертає обрані <see cref="Coords"/>.
+    /// </param>
+    /// <returns><see cref="GameStatus"/> що представляє результат завершеної гри.</returns>
     static abstract GameStatus Play(
         GameSettings gameSettings,
         Action<GameState> drawGame,
