a766deccbfb8a0630b533043282e85046d7c14b0
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-06-28 19:24:50 +0300

    feat(controller): implement IController realisation

--- Reversi.Controller/Controller.cs
+++ Reversi.Controller/Controller.cs
@@ -1,57 +1,139 @@
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using Reversi.Core;
+using Reversi.Network;
+using Reversi.Network.Protocol;
+using NetworkErrorException = Reversi.Core.NetworkErrorException;
 
 namespace Reversi.Controller;
 
-public class Controller<TModel, TView>
+public class Controller<TModel> : IController
     where TModel : IModel, new()
-    where TView : IView, new()
 {
-    private IView _view = new TView();
-
-    public int Main()
+    public static GameState Play(
+        GameSettings gameSettings,
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
     {
-        GameSettings? gameSettings = _view.Start();
-        do
+        try
         {
-            var gameResult = Play(gameSettings.Value);
-            gameSettings = _view.End(gameResult);
-        } while (gameSettings != null);
-
-        return 0;
+            return _Play(gameSettings, drawGame, askMove);
+        }
+        catch (ModelException e)
+        {
+            throw new UnexpectedModelException(e);
+        }
+        catch (DisconnectException e)
+        {
+            throw new OpponentDisconnectedException(e);
+        }
+        catch (NetworkException)
+        {
+            throw new NetworkErrorException();
+        }
+        catch (Exception e)
+        {
+            throw new UnhandledException("Unhandled exception in Controller!", e);
+        }
     }
-
-    [DoesNotReturn]
-    private void FatalError()
-    {
-        Environment.Exit(1);
-    }
-
-    private GameState Play(GameSettings gameSettings)
+    private static GameState _Play(
+        GameSettings gameSettings,
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
     {
         switch (gameSettings.GameType)
         {
             case GameType.Local:
-                return PlayLocalGame();
+                return PlayLocalGame(drawGame, askMove);
             case GameType.NetworkClient:
-                if (gameSettings.ClientSettings == null)
+                if (!gameSettings.ClientSettings.HasValue)
                 {
-                    FatalError();
+                    throw new UnreachableException();
                 }
-                return PlayRemoteGameAsClient(gameSettings.ClientSettings.Value);
+
+                return PlayRemoteGameAsClient(gameSettings.ClientSettings.Value, drawGame, askMove);
             case GameType.NetworkHost:
-                if (gameSettings.HostSettings == null)
+                if (!gameSettings.HostSettings.HasValue)
                 {
-                    FatalError();
+                    throw new UnreachableException();
                 }
-                return PlayRemoteGameAsHost(gameSettings.HostSettings.Value);
+
+                return PlayRemoteGameAsHost(gameSettings.HostSettings.Value, drawGame, askMove);
             default:
                 throw new UnreachableException();
         }
     }
 
-    private GameState PlayLocalGame()
+    private static GameState PlayLocalGame(
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
+    {
+        var model = new TModel();
+        var gameState = model.CurrentState();
+
+        while (true)
+        {
+            Coords[] possibleMoves;
+            possibleMoves = model.PossibleMoves();
+
+            if (possibleMoves.Length == 0)
+            {
+                gameState = model.Pass();
+                continue;
+            }
+
+            drawGame(gameState);
+            var move = askMove(possibleMoves);
+            gameState = model.Move(move);
+
+            if (gameState.CurrentGameStatus != GameStatus.Continue)
+            {
+                return gameState;
+            }
+        }
+    }
+
+    private static GameState PlayRemoteGameAsHost(
+        HostSettings hostSettings,
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
+    {
+        using var host = new Host(
+            System.Net.IPAddress.Parse(hostSettings.Host),
+            hostSettings.Port
+        );
+        host.AcceptConnection(hostSettings.HostPlayer.Opposite());
+
+        var localPlayerMove = hostSettings.HostPlayer == Player.Black;
+
+        return PlayRemoteGame(host, localPlayerMove, drawGame, askMove);
+    }
+
+    private static GameState PlayRemoteGameAsClient(
+        ClientSettings clientSettings,
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
+    {
+        using var client = new Client();
+        var localPlayerColor = client.Connect(
+            System.Net.IPAddress.Parse(clientSettings.Host),
+            clientSettings.Port
+        );
+
+        var localPlayerMove = localPlayerColor == Player.Black;
+        return PlayRemoteGame(client, localPlayerMove, drawGame, askMove);
+    }
+
+    private static GameState PlayRemoteGame(
+        Network.Network network,
+        bool localPlayerMove,
+        Action<GameState> drawGame,
+        Func<Coords[], Coords> askMove
+    )
     {
         var model = new TModel();
         var gameState = model.CurrentState();
@@ -62,35 +144,39 @@ public class Controller<TModel, TView>
 
             if (possibleMoves.Length == 0)
             {
-                model.Pass();
+                gameState = model.Pass();
                 continue;
             }
 
-            var move = DrawGameAndAskMove(gameState, possibleMoves);
+            drawGame(gameState);
+
+            var move = localPlayerMove
+                ? askMove(possibleMoves)
+                : network.ReceiveMessage<MoveMessage>().Coords;
+
+            if (localPlayerMove)
+            {
+                network.Send(new MoveMessage(move));
+                network.ReceiveMessage<StatusOkMessage>();
+            }
+            else
+            {
+                if (!possibleMoves.Contains(move))
+                {
+                    network.Send(new StatusErrorMessage());
+                    throw new NetworkErrorException(new ProtocolException("Impossible move!"));
+                }
+                network.Send(new StatusOkMessage());
+            }
+
             gameState = model.Move(move);
 
             if (gameState.CurrentGameStatus != GameStatus.Continue)
             {
                 return gameState;
             }
+
+            localPlayerMove = !localPlayerMove;
         }
     }
-
-    private GameState PlayRemoteGameAsHost(HostSettings hostSettings)
-    {
-        throw new NotImplementedException();
-    }
-
-    private GameState PlayRemoteGameAsClient(ClientSettings clientSettings)
-    {
-        throw new NotImplementedException();
-    }
-
-    private Coords DrawGameAndAskMove(GameState gameState, Coords[] possibleMoves)
-    {
-        _view.RenderGame(gameState);
-        _view.Draw();
-        return _view.GetMove(possibleMoves);
-    }
 }
-
